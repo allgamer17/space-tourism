2f97d720cc48db83b700cef095d6c3c60b23b94d
Allef Moises <allefmoises17@gmail.com>
2022-11-20 14:30:51 -0300

    responsivida em andamento

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,21 @@
 let menuArea = document.querySelector('.header-navbar-links-group');
 
+function limpar() {
+    menuArea.classList.remove('display-menu hide-menu')
+}
+
 function menuOpener() {
-    if (menuArea.classList.contains('hide-menu') == true) {
-        menuArea.classList.remove('hide-menu')
+    if (menuArea.classList.contains('hide-menu') == false) {
+        menuArea.classList.add('display-menu');
     }else {
-        menuArea.classList.add('display-menu')
+        menuArea.classList.remove('hide-menu');
     }
 };
 
 function menuCloser() {
-    if(menuArea.classList.contains('hide-menu') == false) {
+    if(menuArea.classList.contains('display-menu') == true) {
         menuArea.classList.add('hide-menu');
     }else {
-        menuArea.classList.add('hide-menu');
+        menuArea.classList.remove('display-menu');
     }
 }
